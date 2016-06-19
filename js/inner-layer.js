f3f0751874187524fb5f0db4ae133ab6ab98cc63
zhangquanle <setsaille@gmail.com>
2016-06-19 22:25:34 +0800

    添加页面

---
+++ js/inner-layer.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+	var Odiv=document.getElementById("dssdsa");
+	var Odic=document.getElementById("dic");
+	var Ospana=document.getElementById("spana");
+	Odic.onclick=function(){
+		Odiv.style.display="none";
+		
+	}
+	Ospana.onclick=function(){
+		Odiv.style.display="block";
+
+		
+	}
+}
